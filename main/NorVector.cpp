b3ad5b188eb1c36003d38255772203ae46a68c0e
eritsiandavid <wruser123@gmail.com>
2024-10-03 19:20:28 +0300

    new file:   main/NorVector.cpp 	new file:   main/main.cpp

---
+++ main/NorVector.cpp
@@ -0,0 +1,195 @@
+﻿#include <iostream>
+#include <ctime>
+#include <random>
+
+#define chap size
+#define sarqi_chap make_size
+
+template<typename norVec_Ty>
+class NorVector 
+{
+private:
+	int N;
+	norVec_Ty* norVec;
+	norVec_Ty* arr;
+
+public:
+	NorVector()
+	{
+		N = 0;
+		norVec = new norVec_Ty[N];
+	}
+	NorVector(const int size)
+	{
+		N = size;
+		norVec = new norVec_Ty[N];
+	}
+
+	~NorVector()
+	{
+		delete[] norVec;
+	}
+
+	/**
+	NorVector(const norVec_Ty& other)
+	{
+		this->N = other.size();
+
+		delete this->norVec;
+		this->norVec = new norVec_Ty[N];
+
+		for (int i = 0; i < N; i++)
+		{
+			this->norVec[i] = other[i];
+		}
+	}
+	/**/
+
+	norVec_Ty& operator[] (const int index)
+	{
+		return norVec[index];
+	}
+
+	norVec_Ty& operator= (const norVec_Ty& other)
+	{
+		if (this != &other)
+		{
+			delete[] this->norVec;
+			this->norVec = new norVec_Ty[other.size()];
+
+			for (int i = 0; i < other.size(); i++)
+				this->norVec[i] = other[i];
+
+			this->N = other.N;
+		}
+
+		return *this;
+	}
+
+	std::ostream& operator<<(const norVec_Ty& norV)
+	{
+		std::ostream out;
+
+		for (int i = 0; i < norV.size(); i++)
+			out << norV[i] << " ";
+
+		out << "\n";
+
+		return out;
+	}
+
+	void push_back(const norVec_Ty& value);
+
+	void make_size(const int size);
+
+	void show()
+	{
+		for (int i = 0; i < N; i++)
+			std::cout << norVec[i] << " ";
+
+		std::cout << "\n";
+	}
+
+	void fill_vec()
+	{
+		for (int i = 0; i < N; i++)
+			norVec[i] = rand();
+	}
+
+	void del_vec()
+	{
+		delete[] norVec;
+		norVec = nullptr;
+	}
+
+	void reverse()
+	{
+		for (int i = 0; i < N / 2; i++)
+			std::swap(norVec[i], norVec[N - i - 1]);
+	}
+
+	int size()
+	{
+		return N;
+	}
+};
+
+template<typename norVec_Ty>
+void NorVector<norVec_Ty>::push_back(const norVec_Ty& value)
+{
+	arr = new norVec_Ty[N + 1];
+
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = norVec[i];
+	}
+
+	delete[] norVec;
+
+	norVec = new norVec_Ty[N + 1];
+
+	for (int i = 0; i < N; i++)
+	{
+		norVec[i] = arr[i];
+	}
+
+	delete[] arr;
+	arr = NULL;
+
+	norVec[N] = value;
+	N++;
+}
+
+template<typename norVec_Ty>
+void NorVector<norVec_Ty>::make_size(const int size) 
+{
+	arr = new norVec_Ty[size];
+
+	for (int i = 0; i < N && i < size; i++) 
+	{
+		arr[i] = norVec[i];
+	}
+
+	delete[] norVec;
+
+	norVec = new norVec_Ty[size];
+
+	for (int i = 0; i < size && i < N; i++)
+	{
+		norVec[i] = arr[i];
+	}
+
+	N = size;
+
+	delete[] arr;
+	arr = NULL;
+}
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	NorVector<int> a, b;
+	int lenutsun;
+
+	lenutsun = 10;
+
+	a.make_size(lenutsun);
+	a.fill_vec();
+
+	b.make_size(5);
+	b.fill_vec();
+
+	a.show();
+	b.show();
+	
+	std::cout << "----------------------------------\n";
+
+	a = b;
+
+	a.show();
+	b.show();
+
+	return 0;
+}
---
+++ main/main.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#pragma pack(push, 1)
+
+struct BMPFileHeader 
+{
+    uint16_t file_type{ 0x4D42 };          // Тип файла BMP, константа 0x4D42
+    uint32_t file_size{ 0 };               // Размер файла в байтах
+    uint16_t reserved1{ 0 };               // Зарезервировано, всегда 0
+    uint16_t reserved2{ 0 };               // Зарезервировано, всегда 0
+    uint32_t offset_data{ 0 };             // Начальная позиция пикселя (в байтах от начала файла)
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+
+struct BMPInfoHeader 
+{
+    uint32_t size{ 0 };                      // Размер заголовка в байтах
+    int32_t width{ 0 };                      // Ширина изображения в пикселях
+    int32_t height{ 0 };                     // Высота изображения в пикселях
+    uint16_t planes{ 1 };                    // Метаданные изображения
+    uint16_t bit_count{ 0 };                 // 
+    uint32_t compression{ 0 };               // 
+    uint32_t size_image{ 0 };                //
+    int32_t x_pixels_per_meter{ 0 };         //
+    int32_t y_pixels_per_meter{ 0 };         //
+    uint32_t colors_used{ 0 };               // 
+    uint32_t colors_important{ 0 };          //
+};
+#pragma pack(pop)
+
+BMPFileHeader file_header;
+BMPInfoHeader info;
+vector<unsigned char> raw;
+
+void set_pixel(int x, int y, int r, int g, int b) 
+{
+    raw[3 * (y * info.width + x) + 0] = b;
+    raw[3 * (y * info.width + x) + 1] = g;
+    raw[3 * (y * info.width + x) + 2] = r;
+}
+
+int get_red(int x, int y) 
+{
+    return raw[3 * (y * info.width + x) + 2];
+}
+
+int get_green(int x, int y) 
+{
+    return raw[3 * (y * info.width + x) + 1];
+}
+
+int get_blue(int x, int y) 
+{
+    return raw[3 * (y * info.width + x) + 0];
+}
+
+int main() 
+{
+    ifstream img("floppa.bmp", ios::in | ios::binary);
+    img.read((char*)&file_header, sizeof(file_header));
+    img.read((char*)&info, sizeof(info));
+
+    img.seekg(file_header.offset_data, ios::beg);
+
+    raw.resize(info.width * info.height * info.bit_count / 8);
+    img.read((char*)raw.data(), raw.size());
+
+    for (int y = 20; y < 400; y += 40)
+    {
+        for (int x = 20; x < 400; y += 40)
+        {
+            for (int i = x; i < i + 20; i++)
+            {
+                set_pixel(i, y, 0, 0, 255);
+            }
+        }
+    }
+
+    ofstream out("result.bmp", ios::binary);
+
+    out.write((const char*)&file_header, sizeof(file_header));
+    out.write((const char*)&info, sizeof(info));
+    out.write((const char*)raw.data(), raw.size());
+
+    out.close();
+
+    return 0;
+}
